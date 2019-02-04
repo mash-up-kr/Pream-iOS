42e8ddd9fd38d23d83fa5b9f724741612358f74d
JeaSung Lee <wotjdzz1@naver.com>
2019-02-04 16:13:48 +0900

    library view 화면 구현

--- pream/LibraryView/LibraryViewController.swift
+++ pream/LibraryView/LibraryViewController.swift
@@ -7,11 +7,69 @@
 //
 
 import UIKit
+import Photos
 
 class LibraryViewController: UIViewController {
+    @IBOutlet weak var collectionView: UICollectionView!
+
+    let cellReuseIdentifier = "ImagePickerCollectionViewCell"
+    let columnCount: Int = 3
+    let cellSpacing: CGFloat = 3
+    var fetchResult: PHFetchResult<PHAsset>?
+    var imageManager = PHCachingImageManager()
+    var targetSize = CGSize.zero
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        initView()
+        loadPhotos()
+    }
 
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+    }
+}
+
+extension LibraryViewController {
+    func initView() {
+        let imgWidth = (view.frame.width - (cellSpacing * (CGFloat(columnCount) - 1))) / CGFloat(columnCount)
+        targetSize = CGSize(width: imgWidth, height: imgWidth)
+
+        let layout = UICollectionViewFlowLayout()
+        layout.itemSize = targetSize
+        layout.minimumInteritemSpacing = cellSpacing
+        layout.minimumLineSpacing = cellSpacing
+        collectionView.collectionViewLayout = layout
+
+        collectionView.register(UICollectionViewCell.self, forCellWithReuseIdentifier: cellReuseIdentifier)
+    }
+
+    func loadPhotos() {
+        let options = PHFetchOptions()
+        options.sortDescriptors = [
+            NSSortDescriptor(key: "creationDate", ascending: false)
+        ]
+        fetchResult = PHAsset.fetchAssets(with: .image, options: options)
+    }
+}
+
+extension LibraryViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: cellReuseIdentifier, for: indexPath)
+        guard let fetchResult = fetchResult else { return cell }
+        let photoAsset = fetchResult.object(at: indexPath.item)
+        imageManager.requestImage(for: photoAsset, targetSize: targetSize, contentMode: .aspectFill, options: nil) { image, _ -> Void in
+            let imageView = UIImageView(image: image)
+            imageView.frame.size = cell.frame.size
+            imageView.contentMode = .scaleAspectFill
+            imageView.clipsToBounds = true
+            cell.contentView.addSubview(imageView)
+        }
+        return cell
+    }
+
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return fetchResult?.count ?? 0
     }
 }
