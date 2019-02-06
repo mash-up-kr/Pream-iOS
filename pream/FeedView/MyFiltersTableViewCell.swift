b226e1ac5a937ac291df4e5b649af4a8437569d8
Gaon Kim <gaon.kim.dev@gmail.com>
2019-02-07 05:38:58 +0900

    add: user filters feed

---
+++ pream/FeedView/MyFiltersTableViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  MyFiltersTableViewCell.swift
+//  pream
+//
+//  Created by Gaon Kim on 07/02/2019.
+//  Copyright © 2019 jercy. All rights reserved.
+//
+
+import UIKit
+
+class MyFiltersTableViewCell: UITableViewCell {
+    @IBOutlet weak var collectionView: UICollectionView!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+}
