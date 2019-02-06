212c489c3861d65905b10acb298be3868819e464
Gaon Kim <gaon.kim.dev@gmail.com>
2019-02-06 23:51:23 +0900

    add: feed storyboard, view controller

---
+++ pream/FeedView/FeedViewController.swift
@@ -0,0 +1,16 @@
+//
+//  FeedViewController.swift
+//  pream
+//
+//  Created by Gaon Kim on 06/02/2019.
+//  Copyright © 2019 jercy. All rights reserved.
+//
+
+import UIKit
+
+class FeedViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
