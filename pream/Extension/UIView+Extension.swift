19a90b5e622dd8b0fb1d216137e4098fe0769848
JeaSung Lee <wotjdzz1@naver.com>
2019-02-19 01:30:49 +0900

    filtersettingview

---
+++ pream/Extension/UIView+Extension.swift
@@ -0,0 +1,41 @@
+//
+//  UIView+Border.swift
+//  Bluemoon
+//
+//  Created by 이재성 on 25/11/2018.
+//  Copyright © 2018 mashup6th. All rights reserved.
+//
+
+import UIKit
+
+extension UIView {
+    @IBInspectable var borderWidth: CGFloat {
+        get {
+            return layer.borderWidth
+        }
+        set {
+            layer.borderWidth = newValue
+        }
+    }
+
+    @IBInspectable var borderColor: UIColor? {
+        get {
+            guard let color = layer.borderColor else { return nil }
+            return UIColor(cgColor: color)
+        }
+        set {
+            layer.borderColor = newValue?.cgColor
+        }
+    }
+}
+
+extension UIView {
+    @IBInspectable var cornerRadius: CGFloat {
+        get {
+            return layer.cornerRadius
+        }
+        set {
+            layer.cornerRadius = newValue
+        }
+    }
+}
