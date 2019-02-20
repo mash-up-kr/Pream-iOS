4d64d35d591c3e04237b4f305cb71446f07e9b5d
Suji Kim <rlatnwl9595@naver.com>
2019-02-20 23:19:30 +0900

    add : register filter view

---
+++ pream/DefaultFilterRegisterView/DefaultFilterRegisterViewController.swift
@@ -0,0 +1,21 @@
+//
+//  DefaultFilterRegisterViewController.swift
+//  pream
+//
+//  Created by Suji Kim on 19/02/2019.
+//  Copyright © 2019 jercy. All rights reserved.
+//
+
+import UIKit
+
+class DefaultFilterRegisterViewController: UIViewController {
+    @IBOutlet weak var navigationTitleLabel: UILabel!
+    @IBOutlet weak var previousNavigationTitleLabel:UILabel!
+    @IBOutlet weak var nextNavigationTitleLabel:UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
