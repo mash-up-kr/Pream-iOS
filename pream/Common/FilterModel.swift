1536f478fdb0c592b3a60bd091f4168df6d62032
jercy <wotjdzz1@naver.com>
2019-02-22 17:43:17 +0900

    필터 적용 완료

---
+++ pream/Common/FilterModel.swift
@@ -0,0 +1,149 @@
+//
+//  PRGroupFilter.swift
+//  pream
+//
+//  Created by JERCY on 22/02/2019.
+//  Copyright © 2019 jercy. All rights reserved.
+//
+
+import Foundation
+
+class FilterModel {
+    var groupName: String?
+    var groupImage: UIImage?
+    var groupFilter: GroupFilter = GroupFilter()
+}
+
+class GroupFilter {
+    var gpuExposureFilter: GPUImageExposureFilter = GPUImageExposureFilter()
+    var gpuContrastFilter: GPUImageContrastFilter = GPUImageContrastFilter()
+    var gpuSharpenFilter: GPUImageSharpenFilter = GPUImageSharpenFilter()
+    var gpuSaturationFilter: GPUImageSaturationFilter = GPUImageSaturationFilter()
+    var gpuHighlightFilter: GPUImageHighlightShadowFilter = GPUImageHighlightShadowFilter()
+    var gpuWhiteBalanceFilter: GPUImageWhiteBalanceFilter = GPUImageWhiteBalanceFilter()
+    var gpuVignetteFilter: GPUImageVignetteFilter = GPUImageVignetteFilter()
+    var gpuGrainFilter: GPUImageExposureFilter = GPUImageExposureFilter()
+    var gpuFadeFilter: GPUImageExposureFilter = GPUImageExposureFilter()
+    var gpuSplitToneFilter: GPUImageExposureFilter = GPUImageExposureFilter()
+    lazy var gpuGroupFilter: GPUImageFilterGroup = {
+        let groupFilter = GPUImageFilterGroup()
+        groupFilter.addFilter(gpuExposureFilter)
+        groupFilter.addFilter(gpuContrastFilter)
+        groupFilter.addFilter(gpuSharpenFilter)
+        groupFilter.addFilter(gpuSaturationFilter)
+        groupFilter.addFilter(gpuHighlightFilter)
+        groupFilter.addFilter(gpuWhiteBalanceFilter)
+        groupFilter.addFilter(gpuVignetteFilter)
+        groupFilter.addFilter(gpuGrainFilter)
+        groupFilter.addFilter(gpuFadeFilter)
+        groupFilter.addFilter(gpuSplitToneFilter)
+
+        gpuExposureFilter.addTarget(gpuContrastFilter)
+        gpuContrastFilter.addTarget(gpuSharpenFilter)
+        gpuSharpenFilter.addTarget(gpuSaturationFilter)
+        gpuSaturationFilter.addTarget(gpuHighlightFilter)
+        gpuHighlightFilter.addTarget(gpuWhiteBalanceFilter)
+        gpuWhiteBalanceFilter.addTarget(gpuVignetteFilter)
+        gpuVignetteFilter.addTarget(gpuGrainFilter)
+        gpuGrainFilter.addTarget(gpuFadeFilter)
+        gpuFadeFilter.addTarget(gpuSplitToneFilter)
+
+        groupFilter.initialFilters = [gpuExposureFilter]
+        groupFilter.terminalFilter = gpuSplitToneFilter
+
+        return groupFilter
+    }()
+
+    func getMinMaxValue(filter: Effects, customField: String? = nil) -> (min: Float, max: Float) {
+        switch filter {
+        case .exposure:
+            return (-10, 10)
+        case .contrast:
+            return (0, 4)
+        case .sharpen:
+            return (-4, 4)
+        case .saturation:
+            return (0, 2)
+        case .highlight:
+            return (0, 1)
+        case .shadow:
+            return (0, 1)
+        case .whiteBalance:
+            if customField == "temperature" {
+                return (0, 10000)
+            } else {
+                return (0, 100)
+            }
+        case .vignette:
+            return (0, 1)
+        case .grain:
+            return (0, 0)
+        case .fade:
+            return (0, 0)
+        case .splitTone:
+            return (0, 0)
+        }
+    }
+
+    func setFilter(currentFilter: Effects, customField: String? = nil, value: CGFloat) {
+        switch currentFilter {
+        case .exposure:
+            gpuExposureFilter.exposure = value
+        case .contrast:
+            gpuContrastFilter.contrast = value
+        case .sharpen:
+            gpuSharpenFilter.sharpness = value
+        case .saturation:
+            gpuSaturationFilter.saturation = value
+        case .highlight:
+            gpuHighlightFilter.highlights = value
+        case .shadow:
+            gpuHighlightFilter.shadows = value
+        case .whiteBalance:
+            if customField == "temperature" {
+                gpuWhiteBalanceFilter.temperature = value
+            } else {
+                gpuWhiteBalanceFilter.tint = value
+            }
+        case .vignette:
+            gpuVignetteFilter.vignetteEnd = value
+        case .grain:
+            Log.msg("grain")
+        case .fade:
+            Log.msg("grain")
+        case .splitTone:
+            Log.msg("grain")
+        }
+    }
+
+    func getValue(effect: Effects, customField: String? = nil) -> CGFloat {
+        switch effect {
+        case .exposure:
+            return gpuExposureFilter.exposure
+        case .contrast:
+            return gpuContrastFilter.contrast
+        case .sharpen:
+            return gpuSharpenFilter.sharpness
+        case .saturation:
+            return gpuSaturationFilter.saturation
+        case .highlight:
+            return gpuHighlightFilter.highlights
+        case .shadow:
+            return gpuHighlightFilter.shadows
+        case .whiteBalance:
+            if customField == "temperature" {
+                return gpuWhiteBalanceFilter.temperature
+            } else {
+                return gpuWhiteBalanceFilter.tint
+            }
+        case .vignette:
+            return gpuVignetteFilter.vignetteEnd
+        case .grain:
+            return 0
+        case .fade:
+            return 0
+        case .splitTone:
+            return 0
+        }
+    }
+}
--- pream/FilterSettingView/FilterSettingViewController.swift
+++ pream/FilterSettingView/FilterSettingViewController.swift
@@ -17,68 +17,33 @@ class FilterSettingViewController: UIViewController {
     @IBOutlet weak var bottomSizeView: UIView!
     @IBOutlet weak var effectNameLabel: UILabel!
     @IBOutlet weak var sliderOutlet: UISlider!
+    @IBOutlet weak var temperatureSlider: UISlider!
+    @IBOutlet weak var tintSlider: UISlider!
     @IBOutlet weak var valueLabel: UILabel!
+    @IBOutlet weak var whiteBalanceView: UIView!
+    @IBOutlet weak var temperatureLabel: UILabel!
+    @IBOutlet weak var tintLabel: UILabel!
     var currentFilter: Effects = .exposure
     var currentValue: Float?
     var gpuImage: GPUImagePicture?
-    lazy var gpuGroupFilter: GPUImageFilterGroup = {
-       let groupFilter = GPUImageFilterGroup()
-        groupFilter.addFilter(gpuExposureFilter)
-        groupFilter.addFilter(gpuContrastFilter)
-        groupFilter.addFilter(gpuSharpenFilter)
-        groupFilter.addFilter(gpuSaturationFilter)
-        groupFilter.addFilter(gpuHighlightFilter)
-        groupFilter.addFilter(gpuWhiteBalanceFilter)
-        groupFilter.addFilter(gpuVignetteFilter)
+    var filterModel: FilterModel = FilterModel()
 
-        gpuExposureFilter.addTarget(gpuContrastFilter)
-        gpuContrastFilter.addTarget(gpuSharpenFilter)
-        gpuSharpenFilter.addTarget(gpuSaturationFilter)
-        gpuSaturationFilter.addTarget(gpuHighlightFilter)
-        gpuHighlightFilter.addTarget(gpuWhiteBalanceFilter)
-        gpuWhiteBalanceFilter.addTarget(gpuVignetteFilter)
-
-        groupFilter.initialFilters = [gpuExposureFilter]
-        groupFilter.terminalFilter = gpuVignetteFilter
-
-        return groupFilter
-    }()
-    var gpuExposureFilter: GPUImageExposureFilter = GPUImageExposureFilter()
-    var gpuContrastFilter: GPUImageContrastFilter = GPUImageContrastFilter()
-    var gpuSharpenFilter: GPUImageSharpenFilter = GPUImageSharpenFilter()
-    var gpuSaturationFilter: GPUImageSaturationFilter = GPUImageSaturationFilter()
-    var gpuHighlightFilter: GPUImageHighlightShadowFilter = GPUImageHighlightShadowFilter()
-    var gpuWhiteBalanceFilter: GPUImageWhiteBalanceFilter = GPUImageWhiteBalanceFilter()
-    var gpuVignetteFilter: GPUImageVignetteFilter = GPUImageVignetteFilter()
-
+    @IBAction private func temperatureChangedValue(_ sender: UISlider) {
+        currentValue = sender.value
+        temperatureLabel.text = "\(sender.value)"
+        filterModel.groupFilter.setFilter(currentFilter: currentFilter, customField: "temperature", value: CGFloat(sender.value))
+        gpuImage?.processImage()
+    }
+    @IBAction private func tintChangedValue(_ sender: UISlider) {
+        currentValue = sender.value
+        tintLabel.text = "\(sender.value)"
+        filterModel.groupFilter.setFilter(currentFilter: currentFilter, customField: "tint", value: CGFloat(sender.value))
+        gpuImage?.processImage()
+    }
     @IBAction private func changedValue(_ sender: UISlider) {
         currentValue = sender.value
         valueLabel.text = "\(sender.value)"
-        switch currentFilter {
-        case .exposure:
-            gpuExposureFilter.exposure = CGFloat(sender.value)
-        case .contrast:
-            gpuContrastFilter.contrast = CGFloat(sender.value)
-        case .sharpen:
-            gpuSharpenFilter.sharpness = CGFloat(sender.value)
-        case .saturation:
-            gpuSaturationFilter.saturation = CGFloat(sender.value)
-        case .highlight:
-            gpuHighlightFilter.highlights = CGFloat(sender.value)
-        case .shadow:
-            gpuHighlightFilter.shadows = CGFloat(sender.value)
-        case .whiteBalance:
-            gpuWhiteBalanceFilter.temperature = CGFloat(sender.value)
-        case .vignette:
-            gpuVignetteFilter.vignetteEnd = CGFloat(sender.value)
-        case .grain:
-            Log.msg("grain")
-        case .fade:
-            Log.msg("grain")
-        case .splitTone:
-            Log.msg("grain")
-        }
-
+        filterModel.groupFilter.setFilter(currentFilter: currentFilter, value: CGFloat(sender.value))
         gpuImage?.processImage()
     }
     @IBAction private func closeButtonAction(_ sender: Any) {
@@ -143,8 +108,8 @@ extension FilterSettingViewController: LibraryDelegate {
             let image = UIImage(data: data)
             gpuImage = GPUImagePicture(image: image)
 
-            gpuImage?.addTarget(gpuGroupFilter)
-            gpuGroupFilter.addTarget(previewImageView)
+            gpuImage?.addTarget(filterModel.groupFilter.gpuGroupFilter)
+            filterModel.groupFilter.gpuGroupFilter.addTarget(previewImageView)
             gpuImage?.processImage()
         }
         dismiss(animated: true, completion: nil)
@@ -171,31 +136,28 @@ extension FilterSettingViewController: UICollectionViewDelegate, UICollectionVie
 
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
         guard let effect = Effects(rawValue: indexPath.item) else { return }
-        switch effect {
-        case .exposure:
-            sliderOutlet.value = Float(gpuExposureFilter.exposure)
-        case .contrast:
-            sliderOutlet.value = Float(gpuContrastFilter.contrast)
-        case .sharpen:
-            sliderOutlet.value = Float(gpuSharpenFilter.sharpness)
-        case .saturation:
-            sliderOutlet.value = Float(gpuSaturationFilter.saturation)
-        case .highlight:
-            sliderOutlet.value = Float(gpuHighlightFilter.highlights)
-        case .shadow:
-            sliderOutlet.value = Float(gpuHighlightFilter.shadows)
-        case .whiteBalance:
-            sliderOutlet.value = Float(gpuWhiteBalanceFilter.temperature)
-        case .vignette:
-            sliderOutlet.value = Float(gpuVignetteFilter.vignetteEnd)
-        case .grain:
-            Log.msg("grain")
-        case .fade:
-            Log.msg("fade")
-        case .splitTone:
-            Log.msg("splitTone")
+        if effect == .whiteBalance {
+            whiteBalanceView.isHidden = false
+            let sliderTemperatureValue = filterModel.groupFilter.getMinMaxValue(filter: effect, customField: "temperature")
+            let sliderTintValue = filterModel.groupFilter.getMinMaxValue(filter: effect, customField: "tint")
+            temperatureSlider.minimumValue = sliderTemperatureValue.min
+            temperatureSlider.maximumValue = sliderTemperatureValue.max
+            tintSlider.minimumValue = sliderTintValue.min
+            tintSlider.maximumValue = sliderTintValue.max
+
+            temperatureSlider.value = Float(filterModel.groupFilter.getValue(effect: effect, customField: "temperature"))
+            temperatureLabel.text = "\(filterModel.groupFilter.getValue(effect: effect, customField: "temperature")))"
+            tintSlider.value = Float(filterModel.groupFilter.getValue(effect: effect, customField: "tint"))
+            tintLabel.text = "\(filterModel.groupFilter.getValue(effect: effect, customField: "tint")))"
+        } else {
+            whiteBalanceView.isHidden = true
+            let sliderValue = filterModel.groupFilter.getMinMaxValue(filter: effect)
+            sliderOutlet.minimumValue = sliderValue.min
+            sliderOutlet.maximumValue = sliderValue.max
+
+            sliderOutlet.value = Float(filterModel.groupFilter.getValue(effect: effect))
+            valueLabel.text = "\(filterModel.groupFilter.getValue(effect: effect))"
         }
-        
         filterSettingTopIcon.image = effect.getImage()
         effectNameLabel.text = effect.getText()
         filterSettingTopViewBottomConstraints.constant = 0
