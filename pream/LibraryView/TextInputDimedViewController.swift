84f43ea4d8ef22bc46aee891467aa8b5b7420b69
Gaon Kim <gaon.kim.dev@gmail.com>
2019-02-23 05:40:31 +0900

    Feature/filter list (#45)
    * update: dummy data
    
    * update: storyboard
    
    * update: TextInputDimedVC to inherit KeyboardViewController

--- pream/LibraryView/TextInputDimedViewController.swift
+++ pream/LibraryView/TextInputDimedViewController.swift
@@ -12,17 +12,18 @@ protocol TextInputDimedViewDelegate: class {
     func doneButtonAction(textField: String?)
 }
 
-class TextInputDimedViewController: UIViewController {
+class TextInputDimedViewController: KeyboardViewController {
     var mainTitle: String?
     var message: String?
     @IBOutlet weak var titleLabel: UILabel!
     @IBOutlet weak var messageLabel: UILabel!
-    @IBOutlet weak var inputTexteField: UITextField!
+    @IBOutlet weak var inputTextField: UITextField!
     @IBOutlet weak var doneButton: UIButton!
+    @IBOutlet weak var baseView: UIView!
     weak var delegate: TextInputDimedViewDelegate?
 
     @IBAction private func doneButtonAction(_ sender: Any) {
-        delegate?.doneButtonAction(textField: inputTexteField.text)
+        delegate?.doneButtonAction(textField: inputTextField.text)
     }
 
     @IBAction private func closeButtonAction(_ sender: Any) {
@@ -41,7 +42,24 @@ class TextInputDimedViewController: UIViewController {
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
 
+        inputTextField.delegate = self
         titleLabel.text = mainTitle
         messageLabel.text = message
+    }
+
+    override func keyboardWillShow(notification: NSNotification) {
+        if let keyboardSize = (notification.userInfo?[UIResponder.keyboardFrameBeginUserInfoKey] as? NSValue)?.cgRectValue {
+            let margin = (view.frame.height - baseView.frame.height) / 2
+            if self.view.frame.origin.y == 0, margin < keyboardSize.height {
+                view.frame.origin.y -= keyboardSize.height - margin + 20.0
+            }
+        }
+    }
+}
+
+extension TextInputDimedViewController: UITextFieldDelegate {
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
     }
 }
