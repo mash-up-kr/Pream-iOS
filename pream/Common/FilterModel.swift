6a3c3832f8ac6d0be8d7c9eed640d89cc3c05aba
JeaSung Lee <wotjdzz1@naver.com>
2019-02-23 01:26:03 +0900

    더미데이터 생성

--- pream/Common/FilterModel.swift
+++ pream/Common/FilterModel.swift
@@ -8,10 +8,54 @@
 
 import Foundation
 
+class FilterModelDummy {
+    var dummyFilters: [FilterModel] = []
+
+    func makeDummy() {
+        let dummy1 = FilterModel()
+        let dummy2 = FilterModel()
+        let dummy3 = FilterModel()
+        let dummy4 = FilterModel()
+        let dummy5 = FilterModel()
+
+        let filter1 = GroupFilter()
+        filter1.setFilterValues(exposure: 0.3, contrast: 0.2, sharpness: 0.2, saturation: 0.2, highlights: 0.2, shadows: 0.2, temperature: 5000, tint: 0, vignetteEnd: 0.2, brightness: 0.2)
+        let filter2 = GroupFilter()
+        filter2.setFilterValues(exposure: 0.1, contrast: 0.4, sharpness: 0.3, saturation: 0.1, highlights: 0.5, shadows: 0.5, temperature: 6000, tint: 0, vignetteEnd: 0.1, brightness: 0.3)
+        let filter3 = GroupFilter()
+        filter3.setFilterValues(exposure: 0.5, contrast: 0.2, sharpness: 0.3, saturation: 0.1, highlights: 0.1, shadows: 0.2, temperature: 4000, tint: 0, vignetteEnd: 0.1, brightness: 0.1)
+        let filter4 = GroupFilter()
+        filter4.setFilterValues(exposure: 0.1, contrast: 0.1, sharpness: 0.1, saturation: 0.1, highlights: 0.11, shadows: 0.2, temperature: 5000, tint: 0, vignetteEnd: 0.1, brightness: 0.1)
+        let filter5 = GroupFilter()
+        filter5.setFilterValues(exposure: 0.3, contrast: 0.3, sharpness: 0.3, saturation: 0.3, highlights: 0.3, shadows: 0.3, temperature: 6000, tint: 0, vignetteEnd: 0.3, brightness: 0.3)
+
+        dummy1.makeModel(localId: 1, groupName: "이름", groupImage: nil, groupFilter: filter1)
+        dummy2.makeModel(localId: 2, groupName: "피카", groupImage: nil, groupFilter: filter2)
+        dummy3.makeModel(localId: 3, groupName: "duddj", groupImage: nil, groupFilter: filter3)
+        dummy4.makeModel(localId: 4, groupName: "하하하하하", groupImage: nil, groupFilter: filter4)
+        dummy5.makeModel(localId: 5, groupName: "필터닷", groupImage: nil, groupFilter: filter5)
+
+        dummyFilters.append(dummy1)
+        dummyFilters.append(dummy2)
+        dummyFilters.append(dummy3)
+        dummyFilters.append(dummy4)
+        dummyFilters.append(dummy5)
+    }
+}
+
 class FilterModel {
+    var localId: Int?
+    var serverId: Int?
     var groupName: String?
     var groupImage: UIImage?
     var groupFilter: GroupFilter = GroupFilter()
+
+    func makeModel(localId: Int, groupName: String, groupImage: UIImage?, groupFilter: GroupFilter) {
+        self.localId = localId
+        self.groupName = groupName
+        self.groupImage = groupImage
+        self.groupFilter = groupFilter
+    }
 }
 
 class GroupFilter {
@@ -93,6 +137,22 @@ class GroupFilter {
         case .splitTone:
             return (0, 0)
         }
+    }
+
+    func setFilterValues(exposure: CGFloat, contrast: CGFloat, sharpness: CGFloat, saturation: CGFloat, highlights: CGFloat, shadows: CGFloat, temperature: CGFloat, tint: CGFloat, vignetteEnd: CGFloat, brightness: CGFloat) {
+        gpuExposureFilter.exposure = exposure
+        gpuContrastFilter.contrast = contrast
+        gpuSharpenFilter.sharpness = sharpness
+        gpuSaturationFilter.saturation = saturation
+        gpuHighlightFilter.highlights = highlights
+        gpuHighlightFilter.shadows = shadows
+        gpuWhiteBalanceFilter.temperature = temperature
+        gpuWhiteBalanceFilter.tint = tint
+        gpuVignetteFilter.vignetteEnd = vignetteEnd
+        gpuBrightnessFilter.brightness = brightness
+//        gpuGrainFilter
+//        gpuFadeFilter
+//        gpuSplitToneFilter
     }
 
     func setFilter(currentFilter: Effects, customField: String? = nil, value: CGFloat) {
